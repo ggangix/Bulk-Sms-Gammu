aa34dbf64f835d9d3aaa817cd592bb66f253de54
ggangix <giuseppemiro@gmail.com>
2013-12-18 23:43:17 -0430

    Addingrandom function and output message. Tested and work perfect

--- bulk-sms.sh
+++ bulk-sms.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
+#Developed by: Giuseppe Gangi (@ggangix)
 
-wait_time=3000
-number_list=lista.txt
-TAM=$(wc -l "$number_list" | cut -d" " -f1)
-CONT=1
+#----BEGIN CONFIG----
+number_list=my_list.txt
+cut_line=$(wc -l "$number_list" | cut -d" " -f1)
+count=1
+message="Here my message. "
+wait_time=15 #time to wait for send the next message. (in seconds)
+#----END CONFIG------ 
+
+# Function to generate a Random String to append in each message and avoid restrictions
+randomstring() {
+       randomstringLength=3 
+       rstring=</dev/urandom tr -dc A-Za-z0-9 | head -c $randomstringLength
+       echo $rstring
+}
+
 cat "$number_list" | while read NUMBER
 do
 echo "$NUMBER" > /tmp/sms.tmp
-/echo "$NUMBER" < /tmp/sms.tmp
- sleep ${wait_time}
+echo "--------------------------------------------------------------------"	
+echo "Sending to $NUMBER"
+appendstring=`randomstring`
+echo "$message $appendstring" | gammu -c /etc/gammu-smsdrc --sendsms TEXT $NUMBER
+for a in `seq 1 $wait_time`; do
+    echo -n "$a..." 
+    sleep 1;
+done
+echo ""
+echo "--------------------------------------------------------------------"	
 done
 rm -rf /tmp/sms.tmp
 exit 0
