3f4df6040f1ead093e4ca877b2f8110ddd9dd65a
bgdw <bagus.dwp@gmail.com>
2014-02-25 12:22:05 +0700

    Update bulk-sms.sh
    rearrange config block, add confirmation before sending, fixing counter move it to end of process block

--- bulk-sms.sh
+++ bulk-sms.sh
@@ -2,12 +2,22 @@
 #Developed by: Giuseppe Gangi (@ggangix)
 
 #----BEGIN CONFIG----
-number_list=my_list.txt
-cut_line=$(wc -l "$number_list" | cut -d" " -f1)
-count=1
-message="Here my message. "
+number_list=my_list.txt #file contain phone numbers
+message="Here my message. " #max 155 character
 wait_time=15 #time to wait for send the next message. (in seconds)
+count=1
 #----END CONFIG------ 
+
+cut_line=$(wc -l "$number_list" | cut -d" " -f1)
+total=$(wc -l < "$number_list")
+
+#Confirmation before sending a lot SMS 
+read -p "Are you sure to send $total SMS from $number_list? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
 
 # Function to generate a Random String to append in each message and avoid restrictions
 randomstring() {
@@ -16,13 +26,13 @@ randomstring() {
        echo $rstring
 }
 
+#process
 cat "$number_list" | while read NUMBER
 do
 echo "$NUMBER" > /tmp/sms.tmp
 echo "--------------------------------------------------------------------"	
-count=$((count+1))
 echo "Sending to $NUMBER"
-echo "Queue message number: $count"
+echo "Queue message number: $count of $total"
 appendstring=`randomstring`
 echo "$message $appendstring" | gammu -c /etc/gammu-smsdrc --sendsms TEXT $NUMBER
 for a in `seq 1 $wait_time`; do
@@ -31,6 +41,7 @@ for a in `seq 1 $wait_time`; do
 done
 echo ""
 echo "--------------------------------------------------------------------"	
+count=$((count+1))
 done
 rm -rf /tmp/sms.tmp
 exit 0
